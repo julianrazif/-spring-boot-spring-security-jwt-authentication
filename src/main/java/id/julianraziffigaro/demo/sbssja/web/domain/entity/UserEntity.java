48b9473106b45a49f8e3c54112ef8d52163e6c3e
Julian Razif Figaro <julian.razif@gmail.com>
2022-12-25 13:56:35 +0700

    Add users entity

---
+++ src/main/java/id/julianraziffigaro/demo/sbssja/web/domain/entity/UserEntity.java
@@ -0,0 +1,43 @@
+package id.julianraziffigaro.demo.sbssja.web.domain.entity;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class UserEntity implements Serializable {
+
+  private static final long serialVersionUID = 140423942465064146L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @Column(name = "username")
+  private String username;
+
+  @Column(name = "password")
+  private String password;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+    UserEntity that = (UserEntity) o;
+    return id != null && Objects.equals(id, that.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return getClass().hashCode();
+  }
+}
