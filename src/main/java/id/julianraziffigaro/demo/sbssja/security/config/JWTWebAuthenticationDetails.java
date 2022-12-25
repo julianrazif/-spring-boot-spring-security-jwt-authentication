5129517dadd007ab339176bb99086cbf923f5cb7
Julian Razif Figaro <julian.razif@gmail.com>
2022-12-25 23:56:21 +0700

    Fix JWTWebAuthenticationDetails

--- src/main/java/id/julianraziffigaro/demo/sbssja/security/config/JWTWebAuthenticationDetails.java
+++ src/main/java/id/julianraziffigaro/demo/sbssja/security/config/JWTWebAuthenticationDetails.java
@@ -1,20 +1,22 @@
 package id.julianraziffigaro.demo.sbssja.security.config;
 
+import lombok.EqualsAndHashCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.Serial;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 @Slf4j
+@EqualsAndHashCode(callSuper = false)
 public class JWTWebAuthenticationDetails extends WebAuthenticationDetails {
 
+  @Serial
   private static final long serialVersionUID = -1961230577711811621L;
-
-  private final String authHeader = "Authorization";
 
   private String requestToken;
 
@@ -26,26 +28,14 @@ public class JWTWebAuthenticationDetails extends WebAuthenticationDetails {
   private void init(HttpServletRequest request) {
     Map<String, String> requestHeadersInMap = getRequestHeadersInMap(request);
     Set<Map.Entry<String, String>> entries = requestHeadersInMap.entrySet();
+
     log.info("===================================");
-    log.info("headers start . . .");
-    for (Map.Entry entry : entries) {
-      if (entry.getKey().equals("referer")) {
-        log.info("requestHeaders: key={}, value={}", entry.getKey(), entry.getValue());
-      }
-      if (entry.getKey().equals("origin")) {
-        log.info("requestHeaders: key={}, value={}", entry.getKey(), entry.getValue());
-      }
-      if (entry.getKey().equals("x-forwarded-for")) {
-        log.info("requestHeaders: key={}, value={}", entry.getKey(), entry.getValue());
-      }
-      if (entry.getKey().equals("x-real-ip")) {
-        log.info("requestHeaders: key={}, value={}", entry.getKey(), entry.getValue());
-      }
-    }
+    log.info("headers start...");
+    entries.forEach(entry -> log.info("requestHeaders: key={}, value={}", entry.getKey(), entry.getValue()));
+    log.info("headers end...");
+    log.info("===================================");
 
-    log.info("headers end . . .");
-
-    String headerForAuthentication = request.getHeader(this.authHeader);
+    String headerForAuthentication = request.getHeader("authorization");
 
     if (headerForAuthentication != null && headerForAuthentication.startsWith("Bearer ")) {
       setRequestToken(headerForAuthentication.substring(7));
