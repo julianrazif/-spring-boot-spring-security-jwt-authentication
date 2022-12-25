0f3c08eb1eb9ed67e7d9d518c1cf1b3de3626efe
Julian Razif Figaro <julian.razif@gmail.com>
2022-12-25 23:59:33 +0700

    Fix JWTAuthenticationConverter

--- src/main/java/id/julianraziffigaro/demo/sbssja/security/config/JWTAuthenticationConverter.java
+++ src/main/java/id/julianraziffigaro/demo/sbssja/security/config/JWTAuthenticationConverter.java
@@ -1,55 +1,35 @@
 package id.julianraziffigaro.demo.sbssja.security.config;
 
 import org.springframework.security.authentication.AuthenticationDetailsSource;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationConverter;
-import org.springframework.util.Assert;
 
 import javax.servlet.http.HttpServletRequest;
 
 public class JWTAuthenticationConverter implements AuthenticationConverter {
 
-  private AuthenticationDetailsSource<HttpServletRequest, ?> authenticationDetailsSource;
+  private final AuthenticationDetailsSource<HttpServletRequest, ?> authenticationDetailsSource;
 
-  private final JWTUtils jwtUtils;
-
-  public JWTAuthenticationConverter(JWTUtils jwtUtils) {
-    this(new JWTWebAuthenticationDetailsSource(), jwtUtils);
+  public JWTAuthenticationConverter() {
+    this(new JWTWebAuthenticationDetailsSource());
   }
 
-  public JWTAuthenticationConverter(AuthenticationDetailsSource<HttpServletRequest, ?> authenticationDetailsSource, JWTUtils jwtUtils) {
-    this.authenticationDetailsSource = authenticationDetailsSource;
-    this.jwtUtils = jwtUtils;
-  }
-
-  public AuthenticationDetailsSource<HttpServletRequest, ?> getAuthenticationDetailsSource() {
-    return this.authenticationDetailsSource;
-  }
-
-  public void setAuthenticationDetailsSource(AuthenticationDetailsSource<HttpServletRequest, ?> authenticationDetailsSource) {
-    Assert.notNull(authenticationDetailsSource, "AuthenticationDetailsSource required");
+  public JWTAuthenticationConverter(AuthenticationDetailsSource<HttpServletRequest, ?> authenticationDetailsSource) {
     this.authenticationDetailsSource = authenticationDetailsSource;
   }
+
   @Override
   public Authentication convert(HttpServletRequest request) {
-    try {
-      JWTWebAuthenticationDetails details = (JWTWebAuthenticationDetails) this.authenticationDetailsSource.buildDetails(request);
+    JWTWebAuthenticationDetails details = (JWTWebAuthenticationDetails) this.authenticationDetailsSource.buildDetails(request);
 
-      if (details.getRequestToken() != null) {
+    if (details.getRequestToken() != null) {
+      String token = details.getRequestToken();
+      JWTAuthenticationToken result = new JWTAuthenticationToken(token);
+      result.setDetails(details);
 
-        String token = details.getRequestToken();
-
-        if (jwtUtils.validateToken(token)) {
-          JWTAuthenticationToken result = new JWTAuthenticationToken(token);
-          result.setDetails(details);
-          return result;
-        }
-      }
-
-      return null;
-    } catch (Exception ex) {
-      throw new BadCredentialsException("Invalid token", ex.getCause());
+      return result;
     }
+
+    return null;
   }
 }
